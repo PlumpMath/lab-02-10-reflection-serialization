843d00c0347e9564597294cdedf7103bbf6700c9
Денис Хлякин <mail4aka@gmail.com>
2017-02-11 23:49:33 +0300

    Add deserialization

--- src/com/aka/Main.java
+++ src/com/aka/Main.java
@@ -4,19 +4,31 @@ import com.aka.models.People;
 import com.aka.models.Specials;
 import com.aka.storage.Serializer;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main {
 
-    public static void main(String[] args)
-            throws IOException, IllegalAccessException, TransformerException, ParserConfigurationException {
+    public static void main(String[] args) {
         People people1 = new People("Крис Кельвин", 33, 330000, new Specials(), null);
         People people2 = new People("Хари Кельвин", 33, 330000, new Specials(), null);
+        People people3;
         people1.setSpouse(people2);
         people2.setSpouse(people1);
 
-        Serializer.serialize(people1, System.out);
+        try (FileOutputStream fos = new FileOutputStream("data.xml")) {
+            Serializer.serialize(people1, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (FileInputStream fis = new FileInputStream("data.xml")) {
+            people3 = (People) Serializer.deserialize(fis);
+            Serializer.serialize(people3, System.out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/com/aka/storage/Serializer.java
+++ src/com/aka/storage/Serializer.java
@@ -3,25 +3,39 @@ package com.aka.storage;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.bind.ValidationException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Serializer {
     private final static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    private static final Map<String, Class<?>> namePrimitiveMap = new HashMap<String, Class<?>>();
 
-    public static void serialize(Object o, OutputStream outputStream)
-            throws ParserConfigurationException, TransformerException, IllegalAccessException {
+    static {
+        namePrimitiveMap.put("boolean", Boolean.class);
+        namePrimitiveMap.put("byte", Byte.class);
+        namePrimitiveMap.put("char", Character.class);
+        namePrimitiveMap.put("short", Short.class);
+        namePrimitiveMap.put("int", Integer.class);
+        namePrimitiveMap.put("long", Long.class);
+        namePrimitiveMap.put("double", Double.class);
+        namePrimitiveMap.put("float", Float.class);
+    }
+
+    public static void serialize(Object o, OutputStream outputStream) throws Exception {
         DocumentBuilder db = dbf.newDocumentBuilder();
         final Document doc = db.newDocument();
         final Element root = doc.createElement("object");
@@ -43,6 +57,92 @@ public class Serializer {
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
         transformer.transform(source, result);
+    }
+
+    public static Object deserialize(final InputStream inputStream) throws Exception {
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        final Document doc = db.parse(inputStream);
+
+        if (!"object".equals(doc.getDocumentElement().getTagName()))
+            throw new ValidationException("Document element must be 'object'");
+
+        return loadNode(doc.getDocumentElement());
+    }
+
+    private static Object loadNode(Element node) throws Exception {
+
+        String typeName = node.getAttribute("type");
+        if (typeName.isEmpty())
+            throw new ValidationException("Object type missing");
+
+
+        Class<?> nodeClass = namePrimitiveMap.getOrDefault(typeName, null);
+        if (nodeClass == null)
+            nodeClass = Class.forName(typeName);
+
+        if (isPrimitiveWrapper(nodeClass)) {
+            if (!node.hasAttribute("value")) {
+                throw new ValidationException("Primitive fields must have 'value'");
+            }
+
+            String nodeVal = node.getAttribute("value");
+
+            if (nodeClass != String.class && nodeVal.isEmpty()) {
+                throw new ValidationException("Primitive fields must have non-empty 'value'");
+            }
+
+            if (node.hasAttribute("value")) {
+                if (nodeClass == Character.class) {
+                    if (nodeVal.length() != 1)
+                        throw new ValidationException("Character length must be 1");
+                    return nodeVal.charAt(0);
+                }
+
+                if ((nodeVal.length() > 0) || (nodeClass == String.class)) {
+                    return nodeClass.getConstructor(String.class).newInstance(nodeVal);
+                }
+            }
+            return null;
+        }
+
+        if (node.hasAttribute("value"))
+            throw new ValidationException("Value attribute not allowed for non-primitive types");
+
+        Constructor<?> constructor = null;
+        for (Constructor<?> ctr : nodeClass.getDeclaredConstructors()) {
+            if (ctr.getParameterCount() == 0 && Modifier.isPublic(ctr.getModifiers())) {
+                constructor = ctr;
+                break;
+            }
+        }
+
+        if (constructor == null) {
+            throw new NoSuchMethodException("No-arg constructor req for " + nodeClass.getName());
+        }
+
+        Object obj = constructor.newInstance();
+
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            if (!(node.getChildNodes().item(i) instanceof Element))
+                continue;
+
+            Element fieldNode = (Element) node.getChildNodes().item(i);
+
+            if (!"field".equals(fieldNode.getTagName()))
+                throw new ValidationException("Field elements must be 'field'");
+
+            if (fieldNode.getAttribute("id").equals(""))
+                throw new ValidationException("Field elements must have 'id'");
+
+            Field field = nodeClass.getDeclaredField(fieldNode.getAttribute("id"));
+            int modifiers = field.getModifiers();
+            if (Modifier.isPrivate(modifiers) || Modifier.isProtected(modifiers)) {
+                field.setAccessible(true);
+            }
+            field.set(obj, loadNode(fieldNode));
+        }
+
+        return obj;
     }
 
     private static void appendField(final Document doc, final Element root, Object obj,
@@ -96,5 +196,4 @@ public class Serializer {
                 (type == Float.class) ||
                 (type == String.class);
     }
-
 }
